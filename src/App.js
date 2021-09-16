ced1e14f0f135d0b7b68340391b852a00819ee26
Alexander Chi <arturochi2@hotmail.com>
2021-09-15 19:07:27 -0500

    Se agregó css para móvil y desktop

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@
 **/
 import './assets/sass/App.scss'; 
 import logo from './assets/svgs/logo.svg';
+import { ReactComponent as  IconFacebook} from './assets/svgs/icon-facebook.svg';
+import { ReactComponent as  IconTwitter} from './assets/svgs/icon-twitter.svg';
+import { ReactComponent as  IconInstagram} from './assets/svgs/icon-instagram.svg';
 import illustration from './assets/svgs/illustration-mockups.svg';
 import InputButton from './components/InputButton';
 import Form from './components/Form';
@@ -20,24 +23,34 @@ const App = () => {
     return (
         <section className="container">
             <div className="content">
-                <img alt="Logo Huddle Landing Page" src={logo} width="265" heigth="66" />
-                <div className="left">  
-                    <img alt="" src={illustration} width="710" heigth="510" />
+                <div className="header"> 
+                    <img alt="Logo Huddle Landing Page" src={logo} width="200" heigth="66" />
                 </div>
-                <div className="rigth"> 
-                    <h2>Build The Community</h2>
-                    <h2>Your Fans Will Love</h2>
-                    <p>
-                        Huddle ew-imaginnes the way build communites. You have 
-                        a voices, but so does your audience. Create connections with 
-                        your users as you engage in genuine discussion.
-                    </p>
-                        <InputButton value="Register"/>
-                    <div className="social-medial"> 
-                        <FabLink className="icon facebook"></FabLink>
-                        <FabLink className="icon twitter"></FabLink>
-                        <FabLink className="icon instagram"></FabLink>
+                <div className="body"> 
+                    <div className="col">  
+                        <img className="illustration" alt="Illustration Mockups" src={illustration} width="710" heigth="510" />
                     </div>
+                    <div className="col"> 
+                        <h2>Build The Community</h2>
+                        <h2>Your Fans Will Love</h2>
+                        <p>
+                            Huddle ew-imaginnes the way build communites. You have 
+                            a voices, but so does your audience. Create connections with 
+                            your users as you engage in genuine discussion.
+                        </p>
+                        <InputButton value="Register" handleClick={onSubmit}/> 
+                    </div>
+                </div>
+                <div className="social-medial">  
+                    <FabLink className="facebook"> 
+                        <IconFacebook className="icon" /> 
+                    </FabLink>
+                    <FabLink className="twitter"> 
+                        <IconTwitter className="icon" /> 
+                    </FabLink>
+                    <FabLink className="instagram"> 
+                        <IconInstagram className="icon instagram" /> 
+                    </FabLink>
                 </div>
             </div>
         </section>
